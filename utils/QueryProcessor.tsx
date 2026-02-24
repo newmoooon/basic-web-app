2a25849a16afb440584043142ba1b9a9052d48a0
Cindy Wang <xinyuew4@andrew.cmu.edu>
2026-02-24 15:30:17 +0000

    power

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -79,6 +79,13 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.includes("to the power of")) {
+    const numbers = query.match(/\d+/g)?.map(Number);
+    if (numbers && numbers.length >= 2) {
+      return (numbers[0] ** numbers[1]).toString();
+    }
+  }
+
 
 
 
